d57ccefe63bcccdb75781401eaf4c7a68d9ef523
Tejas Kamble <kambletejas1999@gmail.com>
2025-04-15 22:46:04 +0530

    home

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -5,6 +5,7 @@ import Shop from './pages/shop';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
 
@@ -19,7 +20,9 @@ function App() {
 
   <Route path="/about" element={<About />} />
   <Route path="/contact" element={<Contact />} />
+  
 </Routes>
+<Footer />
 
  
     </>
