c349fea3bda7f3ccdd27a05d9e018c35b5e7c0f5
Tejas Kamble <70898998+tejaskamble99@users.noreply.github.com>
2025-05-12 12:04:31 +0000

    express server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/tejas', (req, res) =>{
+  res.send('hi laude');
+})
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
